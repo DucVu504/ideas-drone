b1f11d04625c7c82a217ffb3367fdadc514b51fb
DucVu <vuduc504@gmail.com>
2024-05-01 01:28:30 +0700

    [FIX] Adjust Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,9 +5,9 @@ import Image from "next/image"
 const Navbar = () => {
     return (
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 relative z-10">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="../puclic/WebGISlogo_2.png" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <Image src="/WebGISlogo_2.png" className="h-8" alt="DroneGIS Logo" width={100} height={200}/>
+        <div className="max-w-screen-xxl flex flex-wrap items-center justify-between mx-auto p-2">
+            <a href="../puclic/Logo_main.png" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <Image src="/Logo_main.png" className="h-16 w-auto p-3" alt="DroneGIS Logo" width={100} height={200}/>
             </a>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get quotes</button>
