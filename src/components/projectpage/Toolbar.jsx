aba250092ab7c038e10f928b311f7b82c856d267
vuduc504 <vuduc504@gmail.com>
2024-06-30 15:45:13 +0700

    [REMOVE] Remove code lines

--- src/components/projectpage/Toolbar.jsx
+++ src/components/projectpage/Toolbar.jsx
@@ -13,9 +13,7 @@ const SearchBar = () => {
           <Image src="/Logo_2.jpg" alt="Logo" width="150" height="50" className="border border-gray-300 rounded-lg"/>
           <span className="text-gray-500 mx-4 px-4 border-x ">DỰ ÁN DEMO</span>
         </div>
-        {/* <div className="flex items-center">
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md">Quay lại</button>
-        </div> */}
+
         <div className="flex items-center">
         <Link href="/user/projects" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md">
             Quay lại
@@ -29,7 +27,6 @@ const SearchBar = () => {
 const ToolBox = () => {
   return (
     <div>
-      {/* <Toolbar/> */}
       <SearchBar/>
     </div>
 
