dc04cf8f9bce28de26a768b863fd03560a52b7e9
@dvu <dvu@gsi-group.asia>
2024-08-30 12:29:10 +0700

    update

--- src/components/pages/user/share/containerWrapper/ContainerWrapper.jsx
+++ src/components/pages/user/share/containerWrapper/ContainerWrapper.jsx
@@ -1,7 +1,7 @@
 const ContainerWrapper = ({ children }) => {
     return (
-        <section className="bg-gray-50 p-3 sm:p-5 lg:ml-36">
-            <div className="mx-auto max-w-screen-xl px-4 lg:mt-16">
+        <section className="bg-gray-50 p-3 sm:p-5 tablet:ml-20 laptop:ml-48 desktop:ml-56">
+            <div className="mx-auto max-w-screen-xl px-4 mt-16">
                 {children}
             </div>
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,16 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'phone': {'max': '640px'}, // Điện thoại
+        'tablet': {'min': '641px', 'max': '1023px'}, // Máy tính bảng
+        'laptop': '1024px',        // Laptop từ 13 inch trở lên
+        'laptop-lg': '1366px',     // Laptop lớn hơn
+        'desktop': '1920px',       // Desktop từ 24 inch trở lên
+        'desktop-lg': '2560px',    // Desktop lớn hơn (27 inch)
+      },
+    },
   },
   plugins: [],
 }
